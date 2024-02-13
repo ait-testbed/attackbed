a78cb9cddda8e721b82e604aa7e4e7901203208e
Wolfgang Hotwagner <wolfgang.hotwagner@ait.ac.at>
2024-02-13 19:31:13 +0000

    added debian-lateral-movement to scenario3

---
+++ terragrunt/repository/module/adminpc_variables.tf
@@ -0,0 +1,17 @@
+variable "adminpc_image" {
+  type        = string
+  description = "image of the adminpc host"
+}
+
+variable "adminpc_flavor" {
+  type        = string
+  description = "flavor of the adminpc host"
+  default     = "m1.small"
+}
+
+variable "adminpc_userdata" {
+  type        = string
+  description = "Userdata for the adminpc virtual machine"
+  default     = null
+}
+
