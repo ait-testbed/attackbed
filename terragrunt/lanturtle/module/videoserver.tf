4f9c68e9103a647f65be67eec4957f4949ff5d0c
annaerdi <erdianna607@gmail.com>
2024-07-17 10:49:49 +0200

    Add terragrunt/lanturtle folder with module files

---
+++ terragrunt/lanturtle/module/videoserver.tf
@@ -0,0 +1,42 @@
+locals {
+    ext_videoserver_userdata_file = var.videoserver_userdata == null ? "${path.module}/scripts/default.yml" : var.videoserver_userdata
+}
+
+####################################################################
+#
+# CREATE INSTANCE for "VIDEOSERVER"
+#
+data "template_file" "userdata_videoserver" {
+  template = "${file("${local.ext_videoserver_userdata_file}")}"
+}
+
+data "template_cloudinit_config" "cloudinitvideoserver" {
+  count         = local.ext_videoserver_userdata_file == null ? 0 : 1
+  gzip          = false
+  base64_encode = false
+
+  part {
+    filename     = "init.cfg"
+    content_type = "text/cloud-config"
+    content      = data.template_file.userdata_videoserver.rendered
+  }
+}
+
+data "openstack_images_image_v2" "videoserver-image" {
+  name        = var.videoserver_image
+  most_recent = true
+}
+
+resource "openstack_compute_instance_v2" "videoserver" {
+  name        = "videoserver"
+  flavor_name = var.videoserver_flavor
+  key_pair    = var.sshkey
+  image_id    = data.openstack_images_image_v2.videoserver-image.id
+  user_data    = local.ext_videoserver_userdata_file == null ? null : data.template_cloudinit_config.cloudinitvideoserver[0].rendered
+
+  network {
+    name = "dmz"
+    fixed_ip_v4 = cidrhost(var.dmz_cidr,121)
+  }
+
+}
