0a5758dcc1e4a7c10a8837198112b4e497da092e
annaerdi <erdianna607@gmail.com>
2024-07-22 18:29:25 +0200

    Add ghostsserver to the terragrunt/lanturtle setup

---
+++ terragrunt/lanturtle/module/ghostsserver.tf
@@ -0,0 +1,42 @@
+locals {
+    ext_ghostsserver_userdata_file = var.ghostsserver_userdata == null ? "${path.module}/scripts/default.yml" : var.ghostsserver_userdata
+}
+
+####################################################################
+#
+# CREATE INSTANCE for "ghostsserver"
+#
+data "template_file" "userdata_ghostsserver" {
+  template = "${file("${local.ext_ghostsserver_userdata_file}")}"
+}
+
+data "template_cloudinit_config" "cloudinitghostsserver" {
+  count         = local.ext_ghostsserver_userdata_file == null ? 0 : 1
+  gzip          = false
+  base64_encode = false
+
+  part {
+    filename     = "init.cfg"
+    content_type = "text/cloud-config"
+    content      = data.template_file.userdata_ghostsserver.rendered
+  }
+}
+
+data "openstack_images_image_v2" "ghostsserver-image" {
+  name        = var.ghostsserver_image
+  most_recent = true
+}
+
+resource "openstack_compute_instance_v2" "ghostsserver" {
+  name        = "ghostsserver"
+  flavor_name = var.ghostsserver_flavor
+  key_pair    = var.sshkey
+  image_id    = data.openstack_images_image_v2.ghostsserver-image.id
+  user_data    = local.ext_ghostsserver_userdata_file == null ? null : data.template_cloudinit_config.cloudinitghostsserver[0].rendered
+
+  network {
+    name = "dmz"
+    fixed_ip_v4 = cidrhost(var.dmz_cidr,122)
+  }
+
+}
