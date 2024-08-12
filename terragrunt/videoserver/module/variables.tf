c7d6d1453d919bdb9363841b488147fce58e4163
thorinaboenke <thorina-boenke@web.de>
2024-08-12 10:15:00 +0200

    add default variables for admin and user cidr

--- terragrunt/videoserver/module/variables.tf
+++ terragrunt/videoserver/module/variables.tf
@@ -8,3 +8,16 @@ variable "dmz_cidr" {
   description = "CIDR of the dmz subnet"
   default     = "172.17.100.0/24"
 }
+
+
+variable "admin_cidr" {
+  type        = string
+  description = "CIDR of the admin subnet"
+  default     = "10.12.0.0/24 "
+}
+
+variable "user_cidr" {
+  type        = string
+  description = "CIDR of the user subnet"
+  default     = "10.11.0.0/16"
+}
