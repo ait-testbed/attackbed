690e37b7c6886e196944bec8638bb821322346d0
thorinaboenke <thorina-boenke@web.de>
2025-03-19 14:34:00 +0100

    attack

--- ansible/run/scenario6/templates/http_server.py
+++ ansible/run/scenario6/templates/http_server.py
@@ -2,16 +2,15 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-LOG_FILE = 'log.txt'
+LOG_FILE = 'stolen_data.txt'
 
 @app.route('/', methods=['POST'])
 def log_post():
     data = request.get_data().decode('utf-8')
     with open(LOG_FILE, 'a') as f:
         f.write(data+ '\n')
-    print(f"Logged: {data}")
-    return 'Logged\n'
-
+    return 
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
 
