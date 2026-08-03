bdaf222be24b3f9fe34fb154d82b167b1ff81777
Erik Grafendorfer <erik.grafendorfer@protonmail.com>
2026-08-03 12:43:33 +0200

    fixed mail address

--- scripts/attacklog_correlator/src/attacklog_correlator/metadata.py
+++ scripts/attacklog_correlator/src/attacklog_correlator/metadata.py
@@ -1,7 +1,7 @@
 """Package metadata."""
 
 __author__ = "Erik Grafendorfer"
-__contact__ = "erik.grafendorfer@ait.ac.at"
+__contact__ = "aecid@ait.ac.at"
 __version__ = "0.1.0"
 
 __all__ = ["__author__", "__contact__", "__version__"]
